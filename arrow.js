a313c8bdb4604c8772d413e258421891df199757
Anda Spataru <spataruanda@yahoo.com>
2023-10-05 11:51:58 +0300

    arrow functions & variables

---
+++ arrow.js
@@ -0,0 +1,17 @@
+function showLog(text){
+  return console.log(text);
+
+}
+
+showLog('Hello world');
+
+const showLog2= (text) => {
+    return console.log(text);
+
+}
+
+showLog2('Hello world');
+
+const showLog3 = text => console.log(text);
+
+showLog3('Hello world');
